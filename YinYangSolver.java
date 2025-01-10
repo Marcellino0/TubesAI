a4e4c102b21732fe0c76f9e90f0e37d2d779baf3
Mrcll <linomarcel399@gmail.com>
2025-01-10 22:49:14 +0700

    Ada bagian yang dihapus

--- YinYangSolver.java
+++ YinYangSolver.java
@@ -349,30 +349,6 @@ public class YinYangSolver {
         
         individual.fitness = calculateFitness(individual.board);
     }
-    private static boolean isValidPosition(int[][] board, int row, int col) {
-        return row >= 0 && row < board.length && col >= 0 && col < board.length;
-    }
-
-    private static class Point {
-        int x, y;
-        
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-        
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point point = (Point) o;
-            return x == point.x && y == point.y;
-        }
-        
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
+   
 }
 
